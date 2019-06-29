e0f9dbdb45982e8fc0581305acc31e2271501fde
maxi <maxi1985.798@gmail.com>
2019-06-29 12:56:46 -0300

    intento de restaurants

--- src/main/java/com/educacionit/delivery/servlet/LoginServlet.java
+++ src/main/java/com/educacionit/delivery/servlet/LoginServlet.java
@@ -20,6 +20,7 @@ import com.educacionit.delivery.dao.DBConnectionManager;
 import com.educacionit.delivery.services.ISecurity;
 import com.educacionit.delivery.services.SecurityException;
 import com.educacionit.delivery.services.support.SecuritySupport;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 
 
@@ -78,7 +79,6 @@ public class LoginServlet extends HttpServlet {
             logger.debug (String.format ("User %s from %s was logged !!!", req.getParameter ("username"), req.getRemoteHost ()));
             HttpSession session = req.getSession ();
             session.setAttribute ("user", u);
-            ArrayList<Restaurant> allRestaurants = getRestaurants();
             // Insert statistic.
             try {
 
@@ -86,11 +86,36 @@ public class LoginServlet extends HttpServlet {
                 Connection conn = db.getConnection ();
 
                 Statement s = conn.prepareStatement ("insert into statistic (id, email, login) values (?, ?, ?)");
+                logger.debug (String.format ("Getting session id for: %s", req.getParameter ("username")));
                 ((PreparedStatement) s).setString (1, session.getId ());
+                logger.debug (String.format ("Getting email for: %s", req.getParameter ("username")));
                 ((PreparedStatement) s).setString (2, u.getEmail ());
+                logger.debug (String.format ("Set Timestamp for: %s", req.getParameter ("username")));
                 ((PreparedStatement) s).setTimestamp(3, new java.sql.Timestamp(new Date().getTime()));
-
+                logger.debug (String.format ("Insert statistic for: %s", req.getParameter ("username")));
                 ((PreparedStatement) s).execute ();
+                
+                // begin get restaurants
+                
+                ArrayList<Restaurant> allRestaurants = new ArrayList<Restaurant>();
+                Statement s2 = conn.createStatement();
+                String queryToGetRestaurants = String.format ("select * from restaurants");
+                logger.debug (String.format ("Executing query: %s", queryToGetRestaurants));
+                ResultSet result = s2.executeQuery (queryToGetRestaurants);
+                while (result.next ()) {
+                    logger.debug (String.format("Restaurant found !!!"));
+                    Restaurant r = new Restaurant();
+                    r.setName (result.getString ("name"));
+                    r.setPhone (result.getString ("phone"));
+                    r.setPhotoLink (result.getString ("photo_link"));
+                    r.setFoods(result.getString ("foods"));
+                    r.setDescription(result.getString ("description"));
+                    allRestaurants.add(r);
+                }
+                
+                session.setAttribute ("restaurants", allRestaurants);
+                
+                // end get restaurants
 
             } catch (Exception e) {
                 logger.error ("Problems saving statistic => " + e.getMessage ());
